fd9f1550818ed707463f36a151a238717a006d7b
ellej <81748770+elle-j@users.noreply.github.com>
2024-10-15 19:13:12 +0200

    Update CLI usage.

--- cli/cli.go
+++ cli/cli.go
@@ -11,41 +11,51 @@ type getCommand struct {
 	flags *flag.FlagSet
 }
 
+const usage = `
+Usage
+  http_client [command] [flags]
+
+Commands
+  get        Make an HTTP GET request and see the response time
+
+Flags
+  -url       The URL to request (e.g. https://example.com)
+  -rounds    The number of requests to make when measuring response times (default 1)`
+
 func newGetCommand() (*getCommand, *string, *int) {
 	command := &getCommand{
 		name:  "get",
-		flags: flag.NewFlagSet("get", flag.ExitOnError),
+		flags: flag.NewFlagSet("get", flag.ContinueOnError),
 	}
 	url := command.flags.String("url", "", "The URL to request (e.g. https://example.com)")
-	rounds := command.flags.Int("rounds", 1, "The number of rounds/iterations to perform the request")
+	rounds := command.flags.Int("rounds", 1, "The number of requests to make when measuring response times")
 
 	return command, url, rounds
-}
-
-func getUsage() string {
-	return "TODO USAGE"
 }
 
 // Start the CLI.
 func Run(args []string) error {
 	if len(args) < 2 {
-		return errors.New(getUsage())
+		return errors.New(usage)
 	}
 
 	command, url, rounds := newGetCommand()
 
 	if args[0] != command.name {
-		return errors.New(getUsage())
+		return errors.New(usage)
 	}
 
-	command.flags.Parse(args[1:])
+	err := command.flags.Parse(args[1:])
+	if err != nil {
+		return err
+	}
 
 	if *url == "" {
-		return errors.New("You must provide a -url flag with a specific URL.\n\n" + getUsage())
+		return errors.New("you must provide a -url flag with a specific URL\n" + usage)
 	}
 
 	if *rounds < 1 {
-		return errors.New("If providing -rounds, it must be a positive number.\n\n" + getUsage())
+		return errors.New("if providing -rounds, it must be a positive integer\n" + usage)
 	}
 
 	measures, err := requestAndMeasure(*url, *rounds)
