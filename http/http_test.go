2890cd2c6e9a8d1c80da3ef100a2b7406c79dc88
ellej <81748770+elle-j@users.noreply.github.com>
2024-10-16 07:02:34 +0200

    Update GET request tests.

--- http/http_test.go
+++ http/http_test.go
@@ -1,8 +1,17 @@
 package http
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
-func TestHttpGet(t *testing.T) {
+func expectContains(t *testing.T, name string, actual string, expected string) {
+	if !strings.Contains(actual, expected) {
+		t.Errorf("expected %s to contain '%s', got: %s", name, expected, actual)
+	}
+}
+
+func TestHttpGetSuccess(t *testing.T) {
 	tests := []string{
 		"gobyexample.com",
 		"https://gobyexample.com/",
@@ -16,10 +25,40 @@ func TestHttpGet(t *testing.T) {
 			response, err := Get(inputUrl)
 
 			if err != nil {
-				t.Errorf("unexpected error: %s", err.Error())
+				t.Errorf("expected no error, got: %s", err.Error())
 			}
 			if len(response) == 0 {
 				t.Error("expected a response")
+			}
+		})
+	}
+}
+
+func TestHttpGetError(t *testing.T) {
+	type testCaseError struct {
+		input                 string
+		expectedErrorContains string
+	}
+
+	tests := []testCaseError{
+		{
+			input:                 "invalidHost",
+			expectedErrorContains: "dial tcp: lookup invalidHost: no such host",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run("GET "+test.input, func(t *testing.T) {
+			response, err := Get(test.input)
+
+			if err == nil {
+				t.Errorf("expected error message to contain '%s', got no error", test.expectedErrorContains)
+			} else {
+				expectContains(t, "error message", err.Error(), test.expectedErrorContains)
+			}
+
+			if len(response) != 0 {
+				t.Errorf("expected no response, got: %s", response)
 			}
 		})
 	}
